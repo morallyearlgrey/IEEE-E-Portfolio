16212867b4e9c0602ade67e9aa69c9cf351bdca3
º • 🌱 kai 🌱 • º <kaisprunger@gmail.com>
2024-10-11 13:36:12 -0400

    Added basic files (:

---
+++ main.js
@@ -0,0 +1,26 @@
+"use strict";
+
+// document.querySelector("#") for selecting
+
+// document.createElement("") to make element
+    // parentNode.appendChild(childnode) appends childNode as last of parentNode
+    // parentNode.insertBefore(newNode, referenceNode) inserts newNode into parentNode before referenceNode
+    // parentNode.removeChild(child) removes child from parentNode on the DOM and returns a reference to child
+
+// node.style.color = ""
+// node.style.cssText = "color: blue; background: white;";
+// node.setAttribute("style", "color: blue; background: white;");
+// node.style.backgroundColor = ""
+// node.getAttribute()
+// node.removeAttribute()
+
+// adds class "new" to your new div
+// div.classList.add("new");
+
+// // removes "new" class from div
+// div.classList.remove("new");
+
+// // if div doesn't have class "active" then add it, or if it does, then remove it
+// div.classList.toggle("active");
+
+// .textContent = "";
